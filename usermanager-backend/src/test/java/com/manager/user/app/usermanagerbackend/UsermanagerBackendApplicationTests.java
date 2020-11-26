e15988405003a1aa9002757487d84d7daf1e0688
davidvera <david.vera@9online.fr>
2020-11-26 08:15:46 +0100

    Initial commit - basic spring boot setup

---
+++ usermanager-backend/src/test/java/com/manager/user/app/usermanagerbackend/UsermanagerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.manager.user.app.usermanagerbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsermanagerBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
