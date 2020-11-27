9426ae095708d8fdb3a6abe80859364ad5b940e2
davidvera <david.vera@9online.fr>
2020-11-27 19:09:01 +0100

    feat(filter): adding authorization filters

---
+++ usermanager-backend/src/main/java/com/manager/user/app/filter/JwtAuthenticationEntryPoint.java
@@ -0,0 +1,40 @@
+package com.manager.user.app.filter;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.manager.user.app.constant.SecurityConstant;
+import com.manager.user.app.domain.HttpResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.Http403ForbiddenEntryPoint;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.OutputStream;
+
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
+
+
+@Slf4j
+@Component
+public class JwtAuthenticationEntryPoint extends Http403ForbiddenEntryPoint {
+    @Override
+    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException arg2)
+            throws IOException {
+        log.debug("Pre-authenticated entry point called. Rejecting access");
+        HttpResponse httpResponse =
+                new HttpResponse(
+                        HttpStatus.FORBIDDEN.value(),
+                        HttpStatus.FORBIDDEN,
+                        HttpStatus.FORBIDDEN.getReasonPhrase().toUpperCase(),
+                        SecurityConstant.ACCESS_DENIED);
+        response.setContentType(APPLICATION_JSON_VALUE);
+        response.setStatus(HttpStatus.FORBIDDEN.value());
+        OutputStream outputStream = response.getOutputStream();
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.writeValue(outputStream, httpResponse);
+        outputStream.flush();
+    }
+}
---
+++ usermanager-backend/src/main/java/com/manager/user/app/filter/JwtAuthorizationFilter.java
@@ -0,0 +1,58 @@
+package com.manager.user.app.filter;
+
+import com.manager.user.app.shared.utils.JwtTokenProvider;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.List;
+
+import static com.manager.user.app.constant.SecurityConstant.OPTIONS_HTTP_METHOD;
+import static com.manager.user.app.constant.SecurityConstant.TOKEN_HEADER;
+
+@Component
+public class JwtAuthorizationFilter extends OncePerRequestFilter {
+    private final JwtTokenProvider jwtTokenProvider;
+
+    @Autowired
+    public JwtAuthorizationFilter(JwtTokenProvider jwtTokenProvider) {
+        this.jwtTokenProvider = jwtTokenProvider;
+    }
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request,
+                                    HttpServletResponse response,
+                                    FilterChain filterChain) throws ServletException, IOException {
+        if (request.getMethod().equalsIgnoreCase(OPTIONS_HTTP_METHOD)) {
+            response.setStatus(HttpStatus.OK.value());
+        } else {
+            String authorizationHeader = request.getHeader(HttpHeaders.AUTHORIZATION);
+            if(authorizationHeader == null || !authorizationHeader.startsWith(TOKEN_HEADER)) {
+                filterChain.doFilter(request, response);
+                return;
+            }
+            String token = authorizationHeader.substring(TOKEN_HEADER.length());
+            String userName = jwtTokenProvider.getSubject(token);
+            if(jwtTokenProvider.isTokenValid(userName, token) && SecurityContextHolder.getContext().getAuthentication() == null) {
+                List<GrantedAuthority> authorities = jwtTokenProvider.getAuthorities(token);
+                Authentication authentication = jwtTokenProvider.getAuthentication(userName, authorities, request);
+                SecurityContextHolder.getContext().setAuthentication(authentication);
+            } else {
+                SecurityContextHolder.clearContext();
+            }
+        }
+        filterChain.doFilter(request, response);
+
+
+    }
+}
