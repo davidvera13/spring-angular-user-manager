358d814d4ebab5587dc79c664f4b02e04c44ef24
davidvera <david.vera@9online.fr>
2020-11-29 23:28:51 +0100

    feat(guard): implementing guards

---
+++ usermanager-frontend/src/app/guard/authentication.guard.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import {CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router} from '@angular/router';
+import { Observable } from 'rxjs';
+import {AuthenticationService} from '../services/authentication/authentication.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthenticationGuard implements CanActivate {
+  constructor(private authenticationService: AuthenticationService,
+              private router: Router) {
+  }
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+    return this.isUserLoggedIn();
+  }
+
+  private isUserLoggedIn(): boolean {
+    if (this.authenticationService.isLoggedIn()) {
+      return true;
+    }
+    this.router.navigate(['/login']);
+    // TODO send notification to user
+    return false;
+  }
+}
